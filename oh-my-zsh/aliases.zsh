cb3c6b914e3c8aacb27a72f877487abd3b6ad001
Guillermo Guerini <guillermoguerini@Guillermos-MacBook-Pro.local>
2018-10-16 08:04:42 -0400

    Customize mac-setup for my development needs.
     - Install oh-my-zsh
     - Install ruby-build and rbenv
     - Custom macOS Defaults
     - Temporarily disable Code customizations and other front end stuff
     - Prefer iTerm over Terminal app
     - Other things I can't remember

---
+++ oh-my-zsh/aliases.zsh
@@ -0,0 +1,10 @@
+# zsh / oh-my-zsh
+alias zshconfig="subl ~/.zshrc"
+alias ohmyzsh="subl ~/.oh-my-zsh"
+alias reload!="source ~/.zshrc"
+
+alias gamd="git commit -a --amend --reuse-message=HEAD"
+alias gwip="git add -u && git commit -m 'WIP'"
+alias gundo="reset HEAD~1 --mixed"
+alias greb="git fetch && git rebase -i origin/master"
+
