3356f573c8d18d61cae759964e5ad5344df08896
Guillermo Guerini <guillermo@gguerini.com>
2026-05-04 15:57:15 -0400

    Clean up aliases.

--- oh-my-zsh/aliases.zsh
+++ oh-my-zsh/aliases.zsh
@@ -3,13 +3,6 @@ alias zshconfig="code ~/.zshrc"
 alias ohmyzsh="code ~/.oh-my-zsh"
 alias reload!="source ~/.zshrc"
 
-alias gamd="git commit -a --amend --reuse-message=HEAD"
-alias gwip="git add -u && git commit -m 'WIP'"
-alias gundo="reset HEAD~1 --mixed"
-alias greb="git fetch && git rebase -i origin/master"
-
 # Show/hide hidden files in Finder
 alias show="defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder"
 alias hide="defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder"
-
-alias cop="bundle exec rubocop"
